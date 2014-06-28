bb5defa6b54da91c53419442cd40eb1460067f9b
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-06-28 15:07:18 -0400

    Use phpunit as a library.

--- build.php
+++ build.php
@@ -8,12 +8,26 @@ if ($returnStatus !== 0) {
     exit(1);
 }
 
+require 'vendor/autoload.php';
+
 passthru('./vendor/bin/phpcs --standard=PSR1 -n src tests *.php', $returnStatus);
 if ($returnStatus !== 0) {
     exit(1);
 }
 
-passthru('./vendor/bin/phpunit --coverage-html coverage --strict tests', $returnStatus);
-if ($returnStatus !== 0) {
+$phpunitConfiguration = PHPUnit_Util_Configuration::getInstance(__DIR__ . '/phpunit.xml');
+$phpunitArguments = array('coverageHtml' => __DIR__ . '/coverage', 'configuration' => $phpunitConfiguration);
+$testRunner = new PHPUnit_TextUI_TestRunner();
+$result = $testRunner->doRun($phpunitConfiguration->getTestSuiteConfiguration(), $phpunitArguments);
+if (!$result->wasSuccessful()) {
     exit(1);
 }
+
+$coverageFactory = new PHP_CodeCoverage_Report_Factory();
+$coverageReport = $coverageFactory->create($result->getCodeCoverage());
+if ($coverageReport->getNumExecutedLines() !== $coverageReport->getNumExecutableLines()) {
+    file_put_contents('php://stderr', "Code coverage was NOT 100%\n");
+    exit(1);
+}
+
+echo "Code coverage was 100%\n";
