6fee3eafc6a72832a0b728a61495df2c14bfdf10
Sathish S <sathish1293@gmail.com>
2019-05-12 12:32:52 +0530

    Add memcache memoize support.

---
+++ src/Memcache.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace TraderInteractive\Memoize;
+
+/**
+ * A memoizer that caches the results in memcache.
+ */
+class Memcache implements Memoize
+{
+    /**
+     * The memcache client
+     *
+     * @var \Memcache
+     */
+    private $client;
+
+    /**
+     * Cache refresh
+     *
+     * @var boolean
+     */
+    private $refresh;
+
+    /**
+     * Sets the memcache client.
+     *
+     * @param \Memcache $client  The memcache client to use
+     * @param boolean         $refresh If true we will always overwrite cache even if it is already set
+     */
+    public function __construct(\Memcache $client, bool $refresh = false)
+    {
+        $this->client = $client;
+        $this->refresh = $refresh;
+    }
+
+    /**
+     * The value is stored in memcache as a json_encoded string,
+     * so make sure that the value you return from $compute is json-encode-able.
+     *
+     * @see Memoize::memoizeCallable
+     *
+     * @param string   $key
+     * @param callable $compute
+     * @param int|null $cacheTime
+     *
+     * @return mixed
+     */
+    public function memoizeCallable(string $key, callable $compute, int $cacheTime = null)
+    {
+        if (!$this->refresh) {
+            try {
+                $cached = $this->client->get($key);
+                if ($cached !== false) {
+                    $data = json_decode($cached, true);
+                    return $data['result'];
+                }
+            } catch (\Exception $e) {
+                return call_user_func($compute);
+            }
+        }
+
+        $result = call_user_func($compute);
+
+        // If the result is false/null/empty, then there is no point in storing it in cache.
+        if ($result === false || $result == null || empty($result))
+            return $result;
+
+        $this->cache($key, json_encode(['result' => $result]), $cacheTime);
+
+        return $result;
+    }
+
+    /**
+     * Caches the value into memcache with errors suppressed.
+     *
+     * @param string $key       The key.
+     * @param string $value     The value.
+     * @param int    $cacheTime The optional cache time
+     *
+     * @return void
+     */
+    private function cache(string $key, string $value, int $cacheTime = null)
+    {
+        try {
+            $this->client->set($key, $value, 0, $cacheTime);
+        } catch (\Exception $e) {
+            // We don't want exceptions in accessing the cache to break functionality.
+            // The cache should be as transparent as possible.
+            // If insight is needed into these exceptions,
+            // a better way would be by notifying an observer with the errors.
+        }
+    }
+}
